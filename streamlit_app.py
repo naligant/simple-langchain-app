961e6295b232e5df7d3b6d2759cd69b84bb11be8
naligant <117927298+naligant@users.noreply.github.com>
2023-10-26 01:07:01 +0000

    Changed title of the app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
-st.title("Welcome to MSU AI Club: Deploying Language Apps!")
+st.title("ChatGPT API Used to Create Chatbot")
 
-st.write("This is a sample demo. If you're following along during the workshop, click \"Fork this app\" on the top right to create your own version of this app.")
+st.write("All rights from AI Club at MSU with minot fixes.")
 
 st.write("For more details, take a look at this workshop's [walkthrough](https://msu-ai.notion.site/LangChain-Apps-MSU-AI-Club-7fbdc17aa0f6415a8d7606cfb1e41e00)")
 
 
 # Uncomment the line below to add a text field, and type "streamlit run streamlit_app.py" in the terminal to see the changes.
 
-st.text_input("Hello")
+#st.text_input("Hello")
